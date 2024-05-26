2ce673100c6f012cc4361b864d6e9002682d3e3a
jonmoreno14 <jonmoreno1498@gmail.com>
2024-05-26 00:06:32 -0600

    testing test

--- card.py
+++ card.py
@@ -8,6 +8,8 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+#test branch#
+
 load_dotenv('.env')
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.default() )
 
